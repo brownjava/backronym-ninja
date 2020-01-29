94808eb10e40ff19f388cdbfd55fc528c93edd42
Jeremy Brown <jeremy@brownjava.org>
2020-01-29 08:48:08 -0800

    Documentation, updating install file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # backronym-ninja
+
+This project is a very simple React app / REST service that implements a backronym generator.
+
+The backronym generator has one feature that other online generators lack: the ability to "pin" words that you like out of a randomly-generated backronym and re-roll the dice on the words that you don't.
+
+The code is hosted live at http://backronym.ninja.
--- install.sh
+++ install.sh
@@ -5,9 +5,7 @@ KEY="backronym.pem"
 ssh -i ${KEY} -L 9229:localhost:9229 ubuntu@${SERVER} "sudo killall -9 node"
 
 rsync -av -e "ssh -i ${KEY}" --delete \
-    --exclude="backronym.pem" --exclude=".git/" --exclude=".gitignore" --exclude=".vscode" --exclude="install.sh" --exclude="config.js" --exclude="apsvideosignaller.service" --exclude="shell_in.sh" \
+    --exclude="backronym.pem" --exclude=".git/" --exclude=".gitignore" --exclude=".vscode" --exclude="install.sh" --exclude="ssh.sh" --exclude="README.md" --exclude="config.js" --exclude="apsvideosignaller.service" --exclude="shell_in.sh" \
     $(dirname ${0}) \
-    ubuntu@${SERVER}:/home/ubuntu/backronym/ # && \
-#    ssh -i ${KEY} -L 9229:localhost:9229 ubuntu@${SERVER} "bash --login -c \"\
-#        nohup sudo node backronym/server/lib/index.js &
-#    \""
+    ubuntu@${SERVER}:/home/ubuntu/backronym/ && \
+ssh -i ${KEY} ubuntu@${SERVER} "bash --login -c \"sudo systemctl restart backronym\""
